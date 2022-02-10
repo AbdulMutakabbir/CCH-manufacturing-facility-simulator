b9b8a46ee9e581e04bd1c1e2a8e143e77ab72dde
Abdul Mutakabbir <47092538+SammSum1@users.noreply.github.com>
2022-02-10 10:39:26 -0500

    added main file

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from Simulator import Simulator
+
+
+# main function for simulator
+def main():
+    sim = Simulator()
+    sim.run_simulation()
+
+
+# run main function
+if __name__ == "__main__":
+    main()
